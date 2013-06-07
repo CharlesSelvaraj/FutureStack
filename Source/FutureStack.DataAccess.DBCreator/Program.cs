29953cd6b29c62bc575ccb60dbdece818e9c0b6f
CharlesSelvaraj <CharlesSelvaraj@live.com>
2013-06-06 21:00:49 -0400

    Renamed and Refactored DBGenerator

--- Source/FutureStack.DataAccess.DBCreator/Program.cs
+++ Source/FutureStack.DataAccess.DBCreator/Program.cs
@@ -1,13 +1,12 @@
-﻿using ServiceStack.OrmLite;
-namespace FutureStack.DataAccess.DBCreator
+﻿namespace FutureStack.DataAccess.DBGenerator
 {
+    using ServiceStack.OrmLite;
     using System.Configuration;
     using System.Data;
     using System.Data.Common;
     using System.Data.SqlClient;
     using DomainModel;
     using ServiceStack.OrmLite.SqlServer;
-
 
     class Program
     {
@@ -16,16 +15,19 @@ namespace FutureStack.DataAccess.DBCreator
             OrmLiteConfig.DialectProvider = new SqlServerOrmLiteDialectProvider();
             DbProviderFactory connectionFactory = SqlClientFactory.Instance;
             IDbConnection dbConnection = connectionFactory.CreateConnection();
-            if (dbConnection != null) dbConnection.ConnectionString = ConfigurationManager.ConnectionStrings["FutureStackSQL"].ToString();
+            if (dbConnection != null) dbConnection.ConnectionString = ConfigurationManager.ConnectionStrings["FutureStackConnectionString"].ToString();
 
             CreateFutureStackDb(dbConnection);
         }
 
         private static void CreateFutureStackDb(IDbConnection dbConnection)
         {
-           if( dbConnection.State != ConnectionState.Open) dbConnection.Open(); 
-           dbConnection.DropAndCreateTable<Customer>();
-           dbConnection.DropAndCreateTable<Address>();
+            if (dbConnection.State != ConnectionState.Open) dbConnection.Open();
+
+            dbConnection.DropAndCreateTable<Customer>();
+            dbConnection.DropAndCreateTable<Address>();
+
+            dbConnection.Close();
         }
     }
 }
